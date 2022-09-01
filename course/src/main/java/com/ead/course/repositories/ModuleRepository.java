04eb4d74c9d3c04d8012b588744fc9531e5d13ee
Mario Miranda <mario_chan15@hotmail.com>
2022-09-01 16:41:45 -0400

    Course Rest API with Validation

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -10,6 +10,6 @@ import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
 
-    @Query(value = "select * from tb_modules where course_course_id = :course_id", nativeQuery = true)
+    @Query(value = "select * from tb_modules where course_course_id = :courseId", nativeQuery = true)
     List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
 }
