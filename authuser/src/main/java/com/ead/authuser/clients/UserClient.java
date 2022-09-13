bf411ef27f5f9d7f9d2a85c187a7e1263715d88d
Mario Miranda <mario_chan15@hotmail.com>
2022-09-13 13:39:55 -0400

    AuthUser - Api Composition Pattern part 2

--- authuser/src/main/java/com/ead/authuser/clients/UserClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -1,10 +1,15 @@
 package com.ead.authuser.clients;
 
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
@@ -17,31 +22,37 @@ import java.util.UUID;
 public class UserClient {
 
     @Autowired
+    UtilsService utilsService;
+    @Autowired
     RestTemplate restTemplate;
-
-    String REQUEST_URI = "http://localhost:8082";
 
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
 
+        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
         List<CourseDto> searchResult = null;
-        String url = REQUEST_URI
-                + "/courses?userId=" + userId
-                + "&page=" + pageable.getPageNumber()
-                + "&size=" + pageable.getPageSize()
-                + "$sort=" + pageable.getSort().toString().replaceAll(": ", ",");
+
+        String url = utilsService.createUrl(userId, pageable);
 
        log.debug("Request URL {}", url);
        log.info("Request URL {}", url);
 
         try {
 
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType =
+                    new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+
+            searchResult = result.getBody().getContent();
+
             log.debug("Response number of elements: {}", searchResult.size());
+
         } catch (HttpStatusCodeException e) {
 
             log.error("Error request /courses {}", e);
         }
+
         log.info("Ending request /courses userId {}", userId);
 
-        return ;
+        return result.getBody();
     }
 }
