d738d868dc96cfe6d0b04417e5f7d95ab0e4f009
Mario Miranda <mario_chan15@hotmail.com>
2022-09-13 14:14:40 -0400

    Course - Api Composition Pattern

---
+++ course/src/main/java/com/ead/course/clients/CourseClient.java
@@ -0,0 +1,59 @@
+package com.ead.course.clients;
+
+import com.ead.course.dtos.ResponsePageDto;
+import com.ead.course.dtos.UserDto;
+import com.ead.course.services.UtilsService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.UUID;
+
+@Log4j2
+@Component
+public class CourseClient {
+
+    @Autowired
+    UtilsService utilsService;
+    @Autowired
+    RestTemplate restTemplate;
+
+    public Page<UserDto> getAllUsersByCourse(UUID courseId, Pageable pageable) {
+
+        ResponseEntity<ResponsePageDto<UserDto>> result = null;
+        List<UserDto> searchResult = null;
+
+        String url = utilsService.createUrl(courseId, pageable);
+
+        log.debug("Request URL {}", url);
+        log.info("Request URL {}", url);
+
+        try {
+
+            ParameterizedTypeReference<ResponsePageDto<UserDto>> responseType =
+                    new ParameterizedTypeReference<ResponsePageDto<UserDto>>() {};
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+
+            searchResult = result.getBody().getContent();
+
+            log.debug("Response number of elements: {}", searchResult.size());
+
+        } catch (HttpStatusCodeException e) {
+
+            log.error("Error request /users {}", e);
+        }
+
+        log.info("Ending request /users courseId {}", courseId);
+
+        return result.getBody();
+    }
+
+}
