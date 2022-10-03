b661832e1a61bfef5f1eee4620ff25beffa72684
Mario Miranda <mario_chan15@hotmail.com>
2022-10-03 11:41:52 -0400

    Circuit-breaker with Resilience4j - Circuit breaker exemple

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,6 +3,7 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +34,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
-    @Retry(name = "retryInstance", fallbackMethod = "retryFallback")
+    @CircuitBreaker(name = "circuitbreakerInstance", fallbackMethod = "circuitbreakerFallback")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
 
         ResponseEntity<ResponsePageDto<CourseDto>> result = null;
@@ -65,7 +66,13 @@ public class CourseClient {
     }
 
     public Page<CourseDto> retryFallback(UUID userId, Pageable pageable, Throwable t) {
-        log.error("Inside retry retryFallback, cause: {}", t.toString());
+        log.error("Inside retry fallback, cause: {}", t.toString());
+        var searchResult = new ArrayList<CourseDto>();
+        return new PageImpl<>(searchResult);
+    }
+
+    public Page<CourseDto> circuitbreakerFallback(UUID userId, Pageable pageable, Throwable t) {
+        log.error("Inside circuit breaker fallback, cause: {}", t.toString());
         var searchResult = new ArrayList<CourseDto>();
         return new PageImpl<>(searchResult);
     }
