def3b08cc7d4960fbf833042f6e2b0db0d98a529
Mario Miranda <mario_chan15@hotmail.com>
2022-10-07 13:50:00 -0400

    Auth User - RoleModel SpringSecurity

--- authuser/src/main/java/com/ead/authuser/services/impl/RoleServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/RoleServiceImpl.java
@@ -1,9 +1,13 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.enums.RoleType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.repositories.RoleRepository;
 import com.ead.authuser.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RoleServiceImpl implements RoleService {
@@ -11,4 +15,8 @@ public class RoleServiceImpl implements RoleService {
     @Autowired
     RoleRepository roleRepository;
 
+    @Override
+    public Optional<RoleModel> findByRoleName(RoleType roleType) {
+        return roleRepository.findByRoleName(roleType);
+    }
 }
