0f39d2f7c1a570b78c59db4cc5c32b25341c562e
Mario Miranda <mario_chan15@hotmail.com>
2022-10-11 10:03:06 -0400

    Course Service - Validate if current user is instructor

--- course/src/main/java/com/ead/course/validation/CourseValidator.java
+++ course/src/main/java/com/ead/course/validation/CourseValidator.java
@@ -1,11 +1,14 @@
 package com.ead.course.validation;
 
+import com.ead.course.configs.security.AuthenticationCurrentUserService;
 import com.ead.course.dtos.CourseDto;
 import com.ead.course.enums.UserType;
 import com.ead.course.models.UserModel;
 import com.ead.course.services.UserService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -13,6 +16,7 @@ import org.springframework.validation.Validator;
 import java.util.Optional;
 import java.util.UUID;
 
+@Log4j2
 @Component
 public class CourseValidator implements Validator {
 
@@ -23,6 +27,8 @@ public class CourseValidator implements Validator {
     @Autowired
     UserService userService;
 
+    @Autowired
+    AuthenticationCurrentUserService authenticationCurrentUserService;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -41,6 +47,13 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors) {
+
+        UUID currentUserId = authenticationCurrentUserService.getCurrentUser().getUserId();
+
+        if (!currentUserId.equals(userInstructor)) {
+            log.warn("User instructor {} does not match with current user {}", userInstructor, currentUserId);
+            throw new AccessDeniedException("Forbidden");
+        }
 
         Optional<UserModel> userModelOptional = userService.findById(userInstructor);
 
